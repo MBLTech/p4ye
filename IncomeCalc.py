eabfbfa20b461ece3d78b8552c3c1025907250d6
Sajid.M <saammaibal@gmail.com>
2021-01-08 19:05:58 +0500

    update

--- IncomeCalc.py
+++ IncomeCalc.py
@@ -1,14 +1,14 @@
 hrs = input("Enter Hours:")
 rate = input("Enter Rate:")
 
-try :
+try:
     h = float(hrs)
     r = float(rate)
-except :
+except:
     print('Invalid Formate')
     quit()
 
-if h > 40 :
+if h > 40:
     otphrs = h - 40
     print('Overtime=', otphrs, 'hrs')
     print('Overtime Rate=', r * 1.5)
@@ -18,4 +18,4 @@ if h > 40 :
 else:
     print('Regular')
     reg = h * r
-    print ('Net Pay:', reg)
+    print('Net Pay:', reg)
