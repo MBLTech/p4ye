606ab3c6f7232dbdedd7bf28efe1292097346999
mbltech <sajidmaibal@gmail.com>
2021-01-20 15:37:44 +0500

    version numbers of projects have been updated

--- projects/SimpleCalcV2.py
+++ projects/SimpleCalcV2.py
@@ -1,3 +1,23 @@
+'''
+ ###############################################
+#    #######################################    #
+####    ########   Simple Calculator   ##########    ####
+#   #######################################     #
+###############################################
+##                                           ##
+  ##########################################
+  ############  Version 2  #################
+  ##########################################
+ ##                                          ##
+'''
+'''
+The version 2 of SimpleCalc.py adds extra functionality but due to my limited
+approach, has to remove to some functions as well, which hope so will be added
+along with these new functions in the later version.
+This version can tell total number of inputs, their summation and and average,
+no matter how many inputs you give it, but as the same time this version unable
+to calculate multiple and devision of the numbers.
+'''
 sum = 0
 count = 0
 
@@ -14,6 +34,6 @@ while True:
     sum = sum + fnum
     count = count + 1
 
-print('total inputs:', count)
-print('Sum:', sum)
-print('average:', sum/count)
+print('----Total Inputs:', count)
+print('----Sum:', sum)
+print('----Average:', sum/count)
