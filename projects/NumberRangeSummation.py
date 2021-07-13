d8fb4478b62b93237c5ba379fecce2df7b0f022a
Sajid .M <sajiddotm@gmail.com>
2021-07-13 16:49:45 +0500

    added fail safe to NRS

--- projects/NumberRangeSummation.py
+++ projects/NumberRangeSummation.py
@@ -1,8 +1,22 @@
-firstvalue = input('Enter Your First Number: ')
-firstvalue = int(firstvalue)
-secondvalue = input('Enter Your Second Number: ')
-secondvalue = int(secondvalue)
-sum = 0
-for i in range (firstvalue,secondvalue):
-    sum = sum + i
-print('Your Total is: ',sum)
+while True:
+    sum = 0
+    firstvalue = input('Enter Your First Number: ')
+    if firstvalue == 'quit' :
+        print('Thankyou for using NRS')
+        break
+    try:
+        firstvalueint = int(firstvalue)
+    except:
+        print('Enter Integers Only')
+        continue
+    secondvalue = input('Enter Your Second Number: ')
+    if secondvalue == 'quit':
+        print('Thankyou for using NRS')
+    try:
+        secondvalueint = int(secondvalue)
+    except:
+        print('Enter Intergers Only')
+        continue
+    for i in range (firstvalueint,secondvalueint):
+        sum = sum + i
+    print('Your Total is: ',sum)
