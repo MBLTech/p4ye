72e91c52931d453ab1ceab915544513d9dca8bfb
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2021-12-04 15:42:15 +0500

    Cleaning code and added better comments for understanding of the code

--- fn_incomecalc.py
+++ fn_incomecalc.py
@@ -3,28 +3,19 @@ Its the 2nd version of Incomecalc.py and made using function statement
 there is nothing new, just the same program with function defined.
 '''
 
-hrs = input("Enter Hours:")
-rate = input("Enter Rate:")
-
-try :
-    h = float(hrs)
-    r = float(rate)
-except :
-    print('Invalid Formate')
+try:
+    hrs = float(input('Enter Hours:'))
+    rph = float(input('Enter Rate Per Hour:'))
+except:
+    print('Invalid Format')
     quit()
 
+# The function to calculate total pay in case of normal as well as when extra hours are served.
 def computepay():
-    if h > 40 :
-        otphrs = h - 40
-        print('Overtime=', otphrs, 'hrs')
-        print('Overtime Rate=', r * 1.5)
-        overtime = (h - 40.0) * (r * 1.5)
-        pay = 40 * r
-        netpay = pay + overtime
+    if hrs <= 40:
+        netpay= hrs * rph # Calculating the net pay if there are no extra hours worked
         return netpay
     else:
-        print('Regular')
-        reg = h * r
-        return reg
-
-print('Pay', computepay())
+        otp = (hrs - 40) * 1.5 * rph + (40 * rph) # The net pay when being worked more than 40 hours calculating overtime with rate of 1.5 times of the orginal rate per hour, and the rest of 40 hours will be calculated as the normal rate per hour of the persion getting payment.
+        return otp
+print("Pay", computepay())
