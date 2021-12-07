5764b5c439d4acdf90c1f03ad0e285e160ae14e3
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2021-12-07 20:18:42 +0500

    spelling mistak

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 '''
 My first program though I have written hello world a lot of times but never
-get past that, due to tthe unconsistancy, but I think with this course and other to
-come this thing gonna change so I will this my first Program anyways
+get past that, due to tthe unconsistancy, but I think with this course and others to
+come, this thing gonna change, so I will write Program anyways
 '''
 print('welcome to the Course of Py4E')
