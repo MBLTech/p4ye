df746491f3e515bfca2b45010ac594cb91dc3256
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2021-12-16 11:29:47 +0500

    spelling mistake

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 '''
-My first program though I have written hello world a lot of times but never
+My first program though I have written hello world a lot of the times but never
 get past that, due to tthe unconsistancy, but I think with this course and others to
 come, this thing gonna change, so I will write this Program anyways.
 '''
