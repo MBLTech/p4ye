7e28a871d10ee19b49fabc18fcc4fe36ae636bed
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2022-01-20 20:00:36 +0500

    fix spelling mistake

--- fn_incomecalc.py
+++ fn_incomecalc.py
@@ -16,6 +16,6 @@ def computepay():
         netpay= hrs * rph # Calculating the net pay if there are no extra hours worked
         return netpay
     else:
-        otp = (hrs - 40) * 1.5 * rph + (40 * rph) # The net pay when being worked more than 40 hours calculating overtime with rate of 1.5 times of the orginal rate per hour, and the rest of 40 hours will be calculated as the normal rate per hour of the persion getting payment.
+        otp = (hrs - 40) * 1.5 * rph + (40 * rph) # The net pay when being worked more than 40 hours calculating overtime with rate of 1.5 times of the original rate per hour, and the rest of 40 hours will be calculated as the normal rate per hour of the persion getting payment.
         return otp
 print("Pay", computepay())
