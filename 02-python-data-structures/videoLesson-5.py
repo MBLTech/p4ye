463bb02b048ec2557061fae640714254bffc47a9
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2022-04-13 20:32:38 +0500

    add solun for video lesson week 6

---
+++ 02-python-data-structures/videoLesson-5.py
@@ -0,0 +1,22 @@
+"""
+Tuples
+"""
+# Limited version of lists but are more efficient in term of memory management
+# Immutable
+# cannot sort, append, pop etc
+# Tuples are comparable
+
+# Sorting Dict by key
+#
+# di = {"a": 24, "c": 2, "d": 10, "b": 6}
+# sort = sorted(di.items())
+# print(sort)
+#
+# Sorting by Value
+#
+# tmp = list()
+# for k, v in di.items():
+#     tmp.append((v, k))
+# print(tmp)
+# stmp = sorted(tmp)
+# print(stmp)
