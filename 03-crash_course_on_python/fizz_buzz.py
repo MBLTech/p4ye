1589aae66df085231335f58645f0ab2d73e8b977
Sajid Mahmood <smonsec+GitHub@proton.me>
2022-09-10 10:27:42 +0500

    add another method for FizzBuzz problem

--- 03-crash_course_on_python/fizz_buzz.py
+++ 03-crash_course_on_python/fizz_buzz.py
@@ -1,10 +1,20 @@
-for i in range (1, 101):
-    if i%3 == 0 and i%5 == 0:
+# for i in range (1, 101):
+#     if i%3 == 0 and i%5 == 0:
+#         print("FizzBuzz")
+#     elif i%3 == 0:
+#         print("Fizz")
+#     elif i%5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+n = 1
+while n <=100:
+    if n%3 == 0 and n%5 == 0:
         print("FizzBuzz")
-    elif i%3 == 0:
+    elif n%3 == 0:
         print("Fizz")
-    elif i%5 == 0:
+    elif n%5 == 0:
         print("Buzz")
     else:
-        print(i)
-
+        print(n)
+    n += 1
